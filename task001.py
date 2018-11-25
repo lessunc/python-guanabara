305c2c30e995c5253b06649fffba23a12e57d459
Leticia Assunção <lefiresnake@gmail.com>
2018-11-25 01:04:15 -0200

    add task001.py

---
+++ task001.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+#--------------------------------------------------
+#  Um programa que retorne a expressão 'Olá,mundo!'
+#--------------------------------------------------
+#  Deixando tudo pronto - Exercício #001
+#--------------------------------------------------
+
+print('Olá,mundo! (primeiro print)')
+
+frase = 'Olá,mundo! (segundo print)'
+print(frase) 	
