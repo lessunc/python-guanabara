6e8fea3801b32db072e2e20d34f194c903e4e897
Leticia Assunção <lefiresnake@gmail.com>
2018-11-27 10:22:51 -0200

    add task005.py

---
+++ task005.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+#------------------------------------------------------------
+#  Um programa que recebe um número inteiro e retorna seu 
+#  número sucessor e antecessor.
+#------------------------------------------------------------
+#  Operadores Aritméticos - Exercício #005
+#------------------------------------------------------------
+
+n = int(input('Digite um número: '))
+print('---' * 22)
+print('Analizando o numero {} seu sucessor é {} e antecessor é {}'.format(n, n+1, n-1))
+print('---' * 22)
+
