33ccbfc3fbb65efd454a6dde3f5a10dcaaf8547b
Leticia Assunção <lefiresnake@gmail.com>
2018-11-28 15:08:35 -0200

    add task008.py + fixes

--- task008.py
+++ task008.py
@@ -1,13 +1,12 @@
 #coding: utf-8
-#------------------------------------------
-#  Um programa que recebe um valor em m² e o
-#  exibe em centímetros e milímetros
-#------------------------------------------
+#--------------------------------------------
+#  Um programa que recebe um valor em m² e 
+#  exibe esse em centímetros e milímetros.
+#--------------------------------------------
+#  Medidas: km hm dam m dm cm mm 
+#--------------------------------------------
 #  Conversor de Medidas - Exercício 008
-#------------------------------------------
-#  >> escala de medidas para auxilio <<
-#  >> km hm dam m dm cm mm <<
-#------------------------------------------
+#--------------------------------------------
 
 m = float(input('Uma distância em metros: '))
 dm = m * 10
@@ -17,7 +16,9 @@ dam = m / 10
 hm = m / 100
 km = m / 1000
 
-print('\033[35m---\033[m' * 22)
+print('\033[35m---\033[m' * 22)#linha colorida(not important)
+
 print(f'A medida de {m:.2f}m² corresponde a {cm}cm {mm}mm')
-print('\033[35m---\033[m' * 22)
+
+print('\033[35m---\033[m' * 22)#linha colorida(not important)
 
