474de3d3919ba3a56d9314f15ed06aeae15ea704
Leticia Assunção <lefiresnake@gmail.com>
2018-11-28 10:30:22 -0200

    add task007.py

---
+++ task007.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+#---------------------------------------------------------
+#  Um programa que recebe duas notas e calcula a média.
+#---------------------------------------------------------
+#  Média Aritmética - Exercício #007
+#---------------------------------------------------------
+
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua outra nota: '))
+
+print('---' *22)
+print('\033[35mSua média total foi: {}\033[m'.format((n1+n2)/2))
+print('---' *22)
+
