28c5cebcacfd2d4b7f5c465d7e6590d1ded3f847
Leticia Assunção <lefiresnake@gmail.com>
2018-12-01 20:45:06 -0200

    add task014.py

---
+++ task014.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+#----------------------------------------------
+#  Um programa que recebe e converte uma 
+#  temperatura em °C para °F.
+#----------------------------------------------
+#  Conversor de Temperaturas - Exercício #014
+#----------------------------------------------
+
+c = float(input('Informe a temperatura em °C : '))
+f = c * 1.8 + 32
+
+print(f'\nA temperatura de {c}° corresponde a {f:.1f}°F')
+print('--' *22)# linha decorativa(not important)
+
