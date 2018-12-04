0b2e8215e9355ee54b21226cbfdeb6893b7c668e
Leticia Assunção <lefiresnake@gmail.com>
2018-12-04 17:33:06 -0200

    add task024.py

--- task024.py
+++ task024.py
@@ -1,10 +1,10 @@
 #coding: utf-8
-#----------------------------------------------------------------
-#  Um programa que recebe o nome de uma cidade e confirma se essa
-#  começa ou não com o nome "Santo".
-#----------------------------------------------------------------
+#-----------------------------------------------------------------
+#  Um programa que recebe o nome de uma cidade e confirma se esse
+#  começa ou não com o nome "Santo", retornando com True ou False.
+#-----------------------------------------------------------------
 #  Verificando as primeiras letras de um texto - Exercício #024
-#----------------------------------------------------------------
+#-----------------------------------------------------------------
 
 c = str(input('Onde você vive?')).strip().lower()
 print(c[:5] == 'santo')
