79353dab07135305b8cf03d846cd74c8a431c516
Leticia Assunção <lefiresnake@gmail.com>
2018-12-11 22:33:01 -0200

    add task046.py

--- task046.py
+++ task046.py
@@ -1,10 +1,10 @@
 #coding: utf-8
-#-----------------------------------------------------------
+#-------------------------------------------------------------
 #  Um programa que mostra na tela uma contagem regressiva  
-#  de 10 à 0 com uma pausa de meio segundo entre eles.
-#-----------------------------------------------------------
+#  de 10 à 0 com uma pausa de meio segundo entre cada número.
+#-------------------------------------------------------------
 #  Contagem Regressiva- Exercício #046
-#-----------------------------------------------------------
+#-------------------------------------------------------------
 from time import sleep
 print('\033[33mContagem Regressiva Para Os Fogos\033[m...')
 
