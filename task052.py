e50d45fe56b6adccb2a9703e2c5345d2242c3bc3
Leticia Assunção <lefiresnake@gmail.com>
2018-12-14 19:15:16 -0200

    add task052.py

---
+++ task052.py
@@ -0,0 +1,29 @@
+#coding: utf-8 
+#----------------------------------------------
+#  Um programa que recebe um número inteiro  
+#  e retorna se esse é ou não um número primo.
+#----------------------------------------------
+#  Números Primos - Exercício #052
+#----------------------------------------------
+tot= 0
+n = int(input('Digite um número para saber se ele é Primo: '))
+
+for c in range(1, n +1):
+	if n % c == 0:
+		print('\033[1;35m', end='')#printa o número com cor
+		tot+= 1
+
+	else:
+		print('\033[m', end='')#printa o número sem cor
+
+	print(f'{c}', end=' ')
+
+print()
+print(f'\033[mO número {n} foi divisível {tot} vezes..')
+
+if tot== 2:
+	print('Número Primo!\n')
+
+else:
+	print('Esse Número Não É Primo!\n')
+
