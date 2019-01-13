29be3bbad46225f13afc6c0a2a33594e414c0cf9
Leticia Assunção <lefiresnake@gmail.com>
2019-01-13 09:55:52 -0200

    add task78.py

--- task078.py
+++ task078.py
@@ -10,32 +10,27 @@ maior = menor = 0
 
 for c in range(0, 5):
 	valores.append(int(input(f'Digite um valor na posição {c+1}: ')))
-
 	if c == 0:
 		maior = menor = valores[0]
 	else: 
 		if valores[c] > maior:
 			maior = valores[c]
-
 		if valores[c] < menor:
 			menor = valores[c]
 
-print('\033[2;35m_._\033[m' *22)# linha colorida(not important)
-
+print('\033[2;35m_._\033[m' *22)#linha decorativa(not important)
 print(f'Valores Digitados: {valores}')		
-print(f'O Maior Valor Digitado Foi: {maior} Nas Posições: ',end=' ')
 
+print(f'O Maior Valor Digitado Foi: {maior} Nas Posições: ',end=' ')
 for e, v in enumerate(valores):
 	if v == maior:
 		print(f'{e +1}...',end='')
 
 print()
 print(f'O Menor Valor Digitado Foi: {menor} Nas Posições: ',end = '')
-
 for e, v in enumerate(valores):
 	if v == menor:
 		print(f'{e +1}...',end=' ')
-
 print()
-print('\033[2;35m_._\033[m' *22)# linha colorida(not important)
+print('\033[2;35m_._\033[m' *22)#linha decorativa(not important)
 
