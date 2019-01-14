a0d986ae162b08069f988fc362869dd63363f076
Leticia Assunção <lefiresnake@gmail.com>
2019-01-14 08:00:00 -0200

    add task081.py

---
+++ task081.py
@@ -0,0 +1,35 @@
+#coding: utf-8
+#--------------------------------------------------------------------------------
+#  Um programa que recebe e cadastra vários números em uma lista, retornando:
+#  • Total de valores | • Lista em ordem decrescente| • Se tem 5 na lista 
+#--------------------------------------------------------------------------------
+#  Extraindo dados de uma Lista - Exercício #081
+#--------------------------------------------------------------------------------
+num = []
+
+while True:
+	opc = ' '
+
+	n_opc = ' '
+	while not n_opc.isdigit():
+		n_opc = input('Digite um valor: ')
+
+	num.append(int(n_opc))
+
+	while opc not in 'SN':
+		opc = str(input('Quer Continuar? [S/N]: ')).strip().upper()[0]
+		print()
+
+	if opc == 'N':
+		break
+
+print(f'Você digitou {len(num)} valores.')
+print(f'Os valores em ordem decrescente são {sorted(num, reverse = True)} ')
+
+if num.count(5) >= 1:
+	print('O número 5 faz parte da lista.')
+else:
+	print('O número 5 não foi encontrado na lista.')
+
+print()
+
