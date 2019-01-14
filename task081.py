8687176c7df260d3fd036c2038523b126fd0f70d
Leticia Assunção <lefiresnake@gmail.com>
2019-01-14 08:17:03 -0200

    add task081.py

--- task081.py
+++ task081.py
@@ -6,30 +6,25 @@
 #  Extraindo dados de uma Lista - Exercício #081
 #--------------------------------------------------------------------------------
 num = []
-
 while True:
 	opc = ' '
-
 	n_opc = ' '
+
 	while not n_opc.isdigit():
 		n_opc = input('Digite um valor: ')
-
 	num.append(int(n_opc))
 
 	while opc not in 'SN':
-		opc = str(input('Quer Continuar? [S/N]: ')).strip().upper()[0]
-		print()
-
+		opc = str(input('\nQuer Continuar? [S/N]: ')).strip().upper()[0]
 	if opc == 'N':
 		break
 
+print('\033[35m~~~\033[m' * 22)# linha decorativa(not important)	
 print(f'Você digitou {len(num)} valores.')
 print(f'Os valores em ordem decrescente são {sorted(num, reverse = True)} ')
 
 if num.count(5) >= 1:
 	print('O número 5 faz parte da lista.')
 else:
-	print('O número 5 não foi encontrado na lista.')
-
-print()
+	print('O número 5 não foi encontrado na lista.\n')
 
