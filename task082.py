a6b6579b097c2806844565af0d1aa1c33e5700ba
Leticia Assunção <lefiresnake@gmail.com>
2019-01-15 09:18:35 -0200

    add task082.py

--- task082.py
+++ task082.py
@@ -1,10 +1,10 @@
 #coding: utf-8
-#--------------------------------------------------------------------------------
-#  Um programa que recebe e cadastra vários números em uma lista, separando os
-#  valores pares em outra lista e a mesma coisa com os números ímpares digitados.
-#--------------------------------------------------------------------------------
+#------------------------------------------------------------------
+#  Um programa que recebe e cadastra vários números em uma lista
+#  separando os valores pares e ímpares em listas diferentes .
+#------------------------------------------------------------------
 #  Dividindo valores em listas - Exercício #082
-#--------------------------------------------------------------------------------
+#------------------------------------------------------------------
 l1 = []
 l2 = []
 l3 = []
@@ -15,23 +15,18 @@ while True:
 
 	while opc not in 'sn':
 		opc = str(input('Quer continuar? [s/n]: '))
-
-		print('{:^26}'.format('\033[34m≈≈≈\033[m'))#linha decorativa(not important)
-
+		print('{:^26}'.format('\033[34m≈≈≈\033[m'))# decorativo(not important)
 	if opc == 'n':
 		break
-
 for c in l1:
 	if c % 2 == 0:
 		l2.append(c)
 	else:
 		l3.append(c)
 
-print('\033[1;34m~~\033[m' *22)# linha decorativa(not important)
-
-print(f'A lista completa é {l1}')
+print(f'\nA lista completa é {l1}')
 print(f'A lista de pares é {l2}')
 print(f'A lista de ímpares é {l3}')
 
-print('\033[1;34m~~\033[m' *22)# linha decorativa(not important)
+print('\033[1;34m~~\033[m' *22)# decorativo(not important)
 
