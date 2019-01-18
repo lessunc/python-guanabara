422b425faa74f770c9114a4875e8679ccda02dad
Leticia Assunção <lefiresnake@gmail.com>
2019-01-18 07:52:33 -0200

    add task087.py

--- task087.py
+++ task087.py
@@ -10,19 +10,16 @@
 #  Mais sobre Matriz em Python - Exercício #087
 #---------------------------------------------------------------------------------
 matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
-somap = somaI = ter = 0
+somap = ter = 0
 
 for m in range(0,3):
 	for c in range(0,3):
 		matriz[m][c] = int(input(f'Digite um valor na posição [{m},{c}]: '))
 		if c == 2:
 			ter += matriz[m][c]
-
 		if matriz[m][c] % 2 == 0:
 			somap += matriz[m][c]
-		else:
-			somaI += matriz[m][c]
-
+		
 print('\n{:~^36}'.format('\033[35mMATRIZ\033[m'))#linha decorativa(not important)
 for m in range(0,3):
 	for c in range(0,3):
@@ -30,8 +27,7 @@ for m in range(0,3):
 	print()
 
 print('~~' *14)
-print(f'\nSoma dos valores pares= {somap}')
-print(f'Soma dos números ímpares= {somaI}')
-print(f'Soma dos números da 3ª coluna= {ter}')
-print(f'Maior valor da 2ª coluna= {max(matriz[1])}\n')
+print(f'\nSoma dos valores pares = {somap}')
+print(f'Soma dos números da 3ª coluna vertical = {ter}')
+print(f'Maior valor da 2ª coluna = {max(matriz[1])}\n')
 
