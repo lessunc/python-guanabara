2934bc17623838fc14af40a84c488aedf127f7a2
Leticia Assunção <lefiresnake@gmail.com>
2019-01-24 21:22:35 -0200

    add task097.py

---
+++ task097.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+#----------------------------------------------------------------------------
+#  Um programa que recebe as dimensões de um terreno retangular (larg, comp)
+#  em uma função, e retorna em seguida a área do terreno.
+#----------------------------------------------------------------------------
+#  Um print especial - Exercício #096
+#----------------------------------------------------------------------------
+def escreva(words):
+	tam = len(words) +4
+	print('-' * tam)
+	print(f'  {words}')
+	print('-' * tam)
+
+escreva('Olá,Mundo!')
+escreva('Curso de Python no YouTube')
+escreva('CEV')
+
