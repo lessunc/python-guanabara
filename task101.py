c77e3f22f0a7bc6bcc15f178da56dcd4e16639f7
Leticia Assunção <lefiresnake@gmail.com>
2019-01-28 21:32:56 -0200

    add task102.py + fixes

--- task101.py
+++ task101.py
@@ -1,4 +1,11 @@
 #coding: utf-8
+#---------------------------------------------------------------------------------
+#  Um programa que contém uma função que recebe como parâmetro o ano de nascimento
+#  de uma pessoa, retornando um valor literal indicando de acordo com sua idade se
+#  essa pessoa tem o voto NEGADO, OPCIONAL, ou OBRIGATÓRIO no ano atual.
+#---------------------------------------------------------------------------------
+#  Funções para votação - Exercício #101
+#---------------------------------------------------------------------------------
 def voto(ano):
 	from datetime import date
 	n = date.today().year - ano
--- task102.py
+++ task102.py
@@ -1,6 +1,19 @@
 #coding: utf-8
-
+#---------------------------------------------------------------------------------
+#  Um programa que contém uma função que recebe dois parâmetros sendo o primeiro
+#  o número a ser calculado, e o outro sendo um valor lógico(opcional) que indica
+#  se o processo do cálculo da fatororial será exibido ou não. Incluso docstring.
+#---------------------------------------------------------------------------------
+#  Função para Fatorial - Exercício #102
+#---------------------------------------------------------------------------------
+print('\n-- FATORIAL DE 5 --')
 def fatorial(n=1, show=False):
+	"""
+	=> Calcula o Fatorial de um número.
+	:parâmetro n: O número a ser calculado.
+	:parâmetro show: (opcional) Mostrar ou não a conta.
+	:return: O valor do Fatorial de um número n.
+	"""
 	f = 1
 	for c in range(n, 0, -1):
 		if show == True:
@@ -11,5 +24,4 @@ def fatorial(n=1, show=False):
 				print('=', end =' ')
 		f *= c
 	return f
-
 print(fatorial(5, True))
