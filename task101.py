4767f6e594549ebcf74a1cd555ecb219a5d10517
Leticia Assunção <lefiresnake@gmail.com>
2019-01-27 22:32:36 -0200

    add task101.py

--- task101.py
+++ task101.py
@@ -1,16 +1,14 @@
 #coding: utf-8
-
 def voto(ano):
 	from datetime import date
 	n = date.today().year - ano
 
 	if n < 16:
-		print(f'Com {n} anos: NÃO VOTA!\n')
+		return f'Com {n} anos: NÃO VOTA!\n'
 	elif 16 <= n < 18 or n > 65:
-		print(f'Com {n} anos: VOTO OPCIONAL!\n')
+		return f'Com {n} anos: VOTO OPCIONAL!\n'
 	else:
-		print(f'Com {n} anos: VOTO OBRIGATÓRIO!\n')
-
+		return f'Com {n} anos: VOTO OBRIGATÓRIO!\n'
 print('---' *11)
-voto(int(input('Ano de Nascimento: ')))
+print(voto(int(input('Ano de Nascimento: '))))
 
