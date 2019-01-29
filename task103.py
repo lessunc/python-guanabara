a841e9c98c1ab1368877cb2133ecabec0629361c
Leticia Assunção <lefiresnake@gmail.com>
2019-01-28 23:21:02 -0200

    add task103.py

--- task103.py
+++ task103.py
@@ -1,11 +1,18 @@
 #coding: utf-8
+#---------------------------------------------------------------------------------
+#  Um programa que contém uma função que recebe dois parâmetros opcionais sendo 
+#  eles o nome do jogador e quantos gols ele marcou. O programa deve mostrar a 
+#  ficha do jogador, mesmo que algum dado não tenha sido informado corretamente
+#---------------------------------------------------------------------------------
+#  Ficha do Jogador - Exercício #103
+#---------------------------------------------------------------------------------
 def ficha(a='<desconhecido>',b=0):
 	
 	print(f'\nO jogador {a} fez {b} gol(s) no campeonato.')
 	print('---' *22)
-
 jog = str(input('Nome do Jogador: '))
 gols = str(input('Número de gols: '))
+
 if gols.isnumeric():
 	gols = int(gols)
 else:
